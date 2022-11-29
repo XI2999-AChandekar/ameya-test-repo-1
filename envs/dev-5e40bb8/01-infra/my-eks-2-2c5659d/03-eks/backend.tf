d3a293710fed1558cafaf36b53ecc08853119d7a
xlr8s <xlr8s@xebia.com>
2022-11-29 11:08:03 +0000

    added my-eks-2-2c5659d resources

---
+++ envs/dev-5e40bb8/01-infra/my-eks-2-2c5659d/03-eks/backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+  backend "s3" {	
+    bucket = "test"
+    encrypt = "true"
+    key = "ameya-test-1-15279f4/envs/dev-5e40bb8/01-infra/my-eks-2-2c5659d/03-eks/terraform.tfstate"
+    region = "af-south-1"
+    role_arn = "arn:aws:iam::123456700001:role/DummyRole1"
+  }
+}
